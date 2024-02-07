daef742b43ea1d172899f0bc9f6ec8c603cd3236
felix <ruiz.felixruiz@gmail.com>
2024-02-07 15:06:51 +0100

    update script

--- config/ollama/start.sh
+++ config/ollama/start.sh
@@ -5,5 +5,6 @@ sleep 30s
 echo "Loading model: $MODEL"
 echo "If this is the first time you are loading this model; this might take while to boot."
 /bin/ollama run $MODEL
-echo "Model $MODEL should be running. It's accessible through the API or in the container";
+echo "Model $MODEL should be running. It's accessible through the API or in the container with command: "
+echo "drc exec ollama ollama run $MODEL";
 while true; do sleep 10h; done
